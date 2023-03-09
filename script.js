9ca6003813eaac207ebb942efee486bbd1b52575
EzzielFawst <76527474+EzzielFawst@users.noreply.github.com>
2023-03-09 16:20:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var elDiceOne = document.getElementById('dice1');
+
+elDiceOne.onclick = function () {rollDice();};
+
+function rollDice() {
+
+  var diceOne = Math.floor((Math.random() * 6) + 1);
+ 
+  console.log(diceOne);
+
+  for (var i = 1; i <= 6; i++) {
+    elDiceOne.classList.remove('show-' + i);
+    if (diceOne === i) {
+      elDiceOne.classList.add('show-' + i);
+    }
+  }
+}
